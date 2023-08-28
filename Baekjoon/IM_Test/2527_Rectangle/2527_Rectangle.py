7be0adc3d7f44e9d7bf1efb9d21136e81d7e5740
RosaDamascena <hsj990604@gmail.com>
2023-08-28 12:48:23 +0900

    0828

---
+++ Baekjoon/IM_Test/2527_Rectangle/2527_Rectangle.py
@@ -0,0 +1,26 @@
+# 2527 - 직사각형
+import sys
+sys.stdin = open('input.txt')
+
+def check():
+    pass
+
+T = 4
+for t in range(T):
+    rectangles = list(map(int, input().split()))
+    ax, ay, ap, aq, bx, by, bp, bq = map(int, rectangles)
+    N = max(rectangles)
+    # a
+    if :
+        pass
+    # b
+    elif :
+        pass
+    # c
+    elif ap == bx and aq == by:
+        print('c')
+    # d
+    elif:
+        pass
+
+    print(draw)
