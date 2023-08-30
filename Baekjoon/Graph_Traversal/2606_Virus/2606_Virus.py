0b12eadf98a8ca7cdce08fb57398cdd5c07438f4
RosaDamascena <hsj990604@gmail.com>
2023-08-30 22:57:22 +0900

    remind problems

--- Baekjoon/Graph_Traversal/2606_Virus/2606_Virus.py
+++ Baekjoon/Graph_Traversal/2606_Virus/2606_Virus.py
@@ -2,23 +2,25 @@
 import sys
 sys.stdin = open('input.txt')
 
+# 1번 컴퓨터를 통해 웜 바이러스에 걸리게 되는 컴퓨터의 수 구하기
 def DFS(node) :
     global cnt
-    visited[node] = True
+    visited[node] = True        # 현재 노드를 방문 처리
 
-    for next in CF[node] :
-        if not visited[next] :
-            DFS(next)
-            cnt += 1
+    for next in CF[node] :      # 현재 노드와 연결된 노드들 확인
+        if not visited[next] :  # 아직 방문하지 않은 노드라면
+            DFS(next)           # 해당 노드로 DFS를 재귀 호출
+            cnt += 1            # DFS가 종료할 때마다 감염된 컴퓨터 수 증가
 
-    return cnt
+    return cnt                  # DFS가 종료될 때마다 감염된 컴퓨터 수 반환
 
-N = int(sys.stdin.readline())
-F = int(sys.stdin.readline())
-CF = [[] for _ in range(N+1)]
-visited = [False] * (N+1)
-cnt = 0
+N = int(sys.stdin.readline())   # 컴퓨터 수
+F = int(sys.stdin.readline())   # 연결되어 있는 컴퓨터 쌍의 수
+CF = [[] for _ in range(N+1)]   # 컴퓨터의 쌍 리스트 생성
+visited = [False] * (N+1)       # 방문 확인 리스트
+cnt = 0                         # 감염된 컴퓨터 수를 저장하는 변수 초기화
 
+# 연결되어 있는 컴퓨터 쌍을 입력받고, CF 리스트에 저장
 for f in range(F) :
     v1, v2 = map(int, sys.stdin.readline().split())
     CF[v1].append(v2)
--- Baekjoon/List/1940_Jumong.py
+++ Baekjoon/List/1940_Jumong.py
@@ -1,26 +1,29 @@
 # 1940 - 주몽
-
+# 투 포인터 알고리즘을 활용하여 갑옷을 만들 수 있는 조합의 개수 계산
 import sys
 
-N = int(sys.stdin.readline())
-M = int(sys.stdin.readline())
+N = int(sys.stdin.readline())   # 재료의 개수
+M = int(sys.stdin.readline())   # 갑옷을 만드는데 필요한 재료의 수
+
+# N개의 재료들이 가진 고유한 번호들
 T = list(map(int, sys.stdin.readline().split()))
 
-cnt = 0
-start = 0
-end = N-1
 
-T.sort()
+cnt = 0                         # 갑옷을 만들 수 있는 조합의 개수 변수 초기화
+start = 0                       # 시작 인덱스
+end = N-1                       # 끝 인덱스
+
+T.sort()    # 재료들을 오름차순으로 정렬
 
 while start < end:
-    current_sum = T[start] + T[end]
-    if current_sum == M:
-        cnt += 1
-        start += 1
-        end -= 1
-    elif current_sum < M:
-        start += 1
-    else:
-        end -= 1
+    current_sum = T[start] + T[end] # 현재 합을 계산
+    if current_sum == M:            # 현재 합이 목표 합과 같다면
+        cnt += 1                    # 갑옷 조합의 개수 증가
+        start += 1                  # 시작 인덱스 +1
+        end -= 1                    # 끝 인덱스 -1
+    elif current_sum < M:           # 현재 합이 목표 합보다 작다면
+        start += 1                  # 시작 인덱스 +1
+    else:                           # 현재 합이 목표 합보다 크다면
+        end -= 1                    # 끝 인덱스 -1
 
 print(cnt)
