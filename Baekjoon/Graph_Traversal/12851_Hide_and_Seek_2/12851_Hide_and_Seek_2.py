e2f75b6467b13dede104244eb94be250d1818831
RosaDamascena <hsj990604@gmail.com>
2023-09-05 21:50:32 +0900

    Hide_and_Seek series 1, 2

--- Baekjoon/Graph_Traversal/12851_Hide_and_Seek_2/12851_Hide_and_Seek_2.py
+++ Baekjoon/Graph_Traversal/12851_Hide_and_Seek_2/12851_Hide_and_Seek_2.py
@@ -1,4 +1,4 @@
-# 1697 - 숨바꼭질
+# 12851 - 숨바꼭질
 import sys
 from collections import deque
 sys.stdin = open('input.txt')
@@ -7,17 +7,23 @@ def BFS(n):
     queue = deque()
     queue.append(n)
     while queue:
+        global min_K, cnt_K
         x = queue.popleft()
         if x == K:
-            break
+            cnt_K += 1
+            continue
         dx = [-1, 1, x]
         for i in range(3):
             nx = x + dx[i]
-            if 0 <= nx < ((10 ** 5) + 2) and not visited[nx]:
-                visited[nx] = visited[x] + 1
-                queue.append(nx)
+            if 0 <= nx < ((10 ** 5) + 2):
+                if not visited[nx] or visited[nx] == visited[x] + 1:
+                    visited[nx] = visited[x] + 1
+                    queue.append(nx)
 
 N, K = map(int, sys.stdin.readline().split())
 visited = [0] * ((10 ** 5) + 2)
+cnt_K = 0
 BFS(N)
+
 print(visited[K])
+print(cnt_K)
