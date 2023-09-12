2a2855ceac93ee8e1adaaec34bec7b314f94adfb
RosaDamascena <hsj990604@gmail.com>
2023-09-12 17:56:04 +0900

    0912

--- Baekjoon/Greedy/15501_Unfair_Puzzle/15501_ Unfair_Puzzle.py
+++ Baekjoon/Greedy/15501_Unfair_Puzzle/15501_ Unfair_Puzzle.py
@@ -6,14 +6,13 @@ input = sys.stdin.readline
 N = int(input())
 nums = list(map(int, input().split()))
 check_nums = list(map(int, input().split()))
-reverse_nums = list(reversed(check_nums))
-answer = 'bad puzzle'
 
-for i in range(N):
-    if nums == check_nums or nums == reverse_nums:
-        answer = 'good puzzle'
-        break
-    check_nums.append(check_nums.pop(0))
-    reverse_nums.append(reverse_nums.pop(0))
+idx = check_nums.index(nums[0])
+check_1 = check_nums[idx:] + check_nums[:idx]
+check_2 = check_nums[idx + 1:] + check_nums[:idx + 1]
+check_2.reverse()
 
-print(answer)
+if nums == check_1 or nums == check_2:
+    print('good puzzle')
+else:
+    print('bad puzzle')
