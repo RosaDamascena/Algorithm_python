5e3e2abd69809f9fb5be07e79ceace3ddb15b323
RosaDamascena <hsj990604@gmail.com>
2023-09-14 17:42:58 +0900

    0914

--- Baekjoon/Graph_Traversal/11724_Connected_Component/11724_Connected_Component.py
+++ Baekjoon/Graph_Traversal/11724_Connected_Component/11724_Connected_Component.py
@@ -1,16 +1,16 @@
 # 11724 - 연결 요소의 개수
 import sys
+sys.setrecursionlimit(10**7)
 sys.stdin = open('input.txt')
 
 N, M = map(int, sys.stdin.readline().split())
 
 def DFS(n):
-    global cnt
-    visited[n] += cnt   # 방문 순서 표시
+    visited[n] = True
     for next in matrix[n]:
         if not visited[next]:
+            visited[next] = True
             DFS(next)
-            cnt += 1
     return visited
 
 matrix = [[] * (N + 1) for i in range(N + 1)]
@@ -21,5 +21,9 @@ for m in range(M):
     matrix[a].append(b)
     matrix[b].append(a)
 
-DFS(1)
-print(visited)
+for i in range(1, N + 1):
+    if not visited[i]:
+        DFS(i)
+        cnt += 1
+
+print(cnt)
